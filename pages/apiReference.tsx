16524491ca01886c87fea6984e0fecb28bb4bc7b
Keagan Pang <keagan.pang@y42.com>
2023-01-16 21:23:27 +0100

    feat: styled api reference page

--- pages/apiReference.tsx
+++ pages/apiReference.tsx
@@ -7,13 +7,41 @@ export default function ApiReference(props) {
   return (
     <div>
       <div className="custom-nav-container sticky top-0 z-20 w-full bg-transparent">
-        <nav className="h-12 bg-[#111] text-slate-50">
+        <nav className="h-12 bg-[#111] p-4 border-b-[2px] border-b-[#212121] text-[#d1d1d1] text-sm flex flex-row items-center">
           <Link href="/"> Back </Link>
         </nav>
       </div>
 
-      <main className="relative bg-white">
-        <RedocStandalone spec={props.openApiSpec} />
+      <main className="relative bg-[#111]">
+        <RedocStandalone
+          spec={props.openApiSpec}
+          options={{
+            hideLoading: true,
+            theme: {
+              colors: {
+                primary: { main: "#6E26EC" },
+                http: {},
+                responses: {},
+                text: {
+                  primary: "#d1d1d1",
+                  secondary: "#ffffff52",
+                },
+              },
+              sidebar: {
+                backgroundColor: "#111",
+                textColor: "#737373",
+                activeTextColor: "#6E26EC",
+              },
+              rightPanel: { backgroundColor: "#2f2338", textColor: "#737373" },
+              fab: { backgroundColor: "#d1d1d1" },
+              schema: {
+                typeTitleColor: "#ffffff52",
+                requireLabelColor: "#E35755",
+              },
+              typography: { code: { color: "#ffff10" } },
+            },
+          }}
+        />
       </main>
     </div>
   );
