34e361d79df323daa97241b0b6bf37e22eef2c62
Keagan Pang <keagan.pang@y42.com>
2023-01-17 20:15:57 +0100

    split setup mdx into auth and config for dynamic api routes

--- pages/api/setup-guides/[sourceName].tsx
+++ pages/api/setup-guides/[sourceName].tsx
@@ -4,21 +4,41 @@ import { join } from "path";
 import { remark } from "remark";
 import html from "remark-html";
 
+// export default async function handler(req, res) {
+//   const { sourceName } = req.query;
+//   const result = await parseMarkdown(sourceName);
+//   res.status(200).json(result);
+// }
+
+// export async function parseMarkdown(sourceName) {
+//   const filePath = join(
+//     process.cwd(),
+//     `/pages/sources/connectors/${sourceName}/${sourceName}-setup-guide.mdx`
+//   );
+//   const fileContents = fs.readFileSync(filePath, "utf-8");
+
+//   const { data, content } = matter(fileContents);
+//   const result = await remark().use(html).process(content);
+
+//   return { data: data, content: result.toString() };
+// }
+
 export default async function handler(req, res) {
   const { sourceName } = req.query;
-  const result = await parseMarkdown(sourceName);
-  res.status(200).json(result);
+  const auth = await parseMarkdown(sourceName, "authenticate");
+  const config = await parseMarkdown(sourceName, "configure");
+  res.status(200).json({ ...auth, ...config });
 }
 
-export async function parseMarkdown(sourceName) {
+export async function parseMarkdown(sourceName, suffix) {
   const filePath = join(
     process.cwd(),
-    `/pages/sources/connectors/${sourceName}/${sourceName}-setup-guide.mdx`
+    `/pages/sources/connectors/${sourceName}/${sourceName}-${suffix}.mdx`
   );
   const fileContents = fs.readFileSync(filePath, "utf-8");
 
   const { data, content } = matter(fileContents);
   const result = await remark().use(html).process(content);
 
-  return { data: data, content: result.toString() };
+  return { [suffix]: result.toString() };
 }
