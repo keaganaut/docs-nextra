775970be5cf4feedc02ad4dfc77f6546724e31ef
Keagan Pang <keagan.pang@y42.com>
2023-01-18 13:52:50 +0100

    fixed cache-control for api/setup-guides/{sourceName}

--- pages/api/setup-guides/[sourceName].tsx
+++ pages/api/setup-guides/[sourceName].tsx
@@ -4,18 +4,22 @@ import { join } from "path";
 import { remark } from "remark";
 import html from "remark-html";
 
-export default async function handler(req, res) {
+import type { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+): Promise<void> {
   res.setHeader(
     "Cache-Control",
-    "s-maxage=86400",
-    "stale-while-revalidate=1296000"
+    "s-maxage=86400, stale-while-revalidate=1296000"
   );
   const { sourceName } = req.query;
-  const result = await parseMarkdown(sourceName);
+  const result = await parseMarkdown(sourceName as string);
   res.status(200).json(result);
 }
 
-export async function parseMarkdown(sourceName) {
+export async function parseMarkdown(sourceName: string) {
   const filePath = join(
     process.cwd(),
     `/pages/sources/connectors/${sourceName}/${sourceName}-setup-guide.mdx`
