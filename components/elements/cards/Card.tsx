fdf02f401a2e55ee57a60996f5916ff630c2389d
Keagan Pang <keagan.pang@y42.com>
2023-02-26 20:39:18 +0100

    added design token values to tailwind config

--- components/elements/cards/Card.tsx
+++ components/elements/cards/Card.tsx
@@ -15,7 +15,7 @@ export const Card = ({ icon, title, description, href }: CardProps) => {
   return (
     <Link
       href={href}
-      className="card group flex flex-col col-span-12 md:col-span-6 lg:col-span-4 p-6 rounded-lg justify-start overflow-hidden border border-gray-200 dark:border-gray-600"
+      className="card group flex flex-col col-span-12 md:col-span-6 lg:col-span-4 p-6 rounded-lg justify-start overflow-hidden border border-gray-100 dark:border-gray-300"
     >
       <div className="content-icon">{icon}</div>
       <h3 className="content-title font-semibold mt-4 mb-1">{title}</h3>
