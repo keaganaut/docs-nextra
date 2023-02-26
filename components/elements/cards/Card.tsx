85aef3dd5ab522db6c3b6bb33bcd9d2d94cc76f6
Keagan Pang <keagan.pang@y42.com>
2023-02-26 20:01:07 +0100

    added cards and card components

--- components/elements/cards/Card.tsx
+++ components/elements/cards/Card.tsx
@@ -1,84 +1,25 @@
-export const Card = () => {
+import Link from "next/link";
+import { ReactNode } from "react";
+interface CardProps {
+  icon: ReactNode;
+  title: string;
+  description: string;
+  href: string;
+}
+
+export const Cards = ({ children }) => {
+  return <div className="grid grid-cols-12 gap-6 my-6">{children}</div>;
+};
+
+export const Card = ({ icon, title, description, href }: CardProps) => {
   return (
-    <div className="grid grid-cols-12 gap-4 my-4">
-      <div className="col-span-12 md:col-span-6 lg:col-span-4">
-        <div className="card group flex flex-col p-6 rounded-lg justify-start overflow-hidden border border-red">
-          <div className="content-icon">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="w-8 h-8"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M21 7.5l-9-5.25L3 7.5m18 0l-9 5.25m9-5.25v9l-9 5.25M3 7.5l9 5.25M3 7.5v9l9 5.25m0-9v9"
-              />
-            </svg>
-          </div>
-          <h3 className="content-title font-semibold mt-4 mb-2">
-            Orchestration
-          </h3>
-          <span className="content-description">
-            Deploy our Orchestration template, or view a live example
-          </span>
-        </div>
-      </div>
-      <div className="col-span-12 md:col-span-6 lg:col-span-4">
-        <div className="card group flex flex-col p-6 rounded-lg justify-start overflow-hidden border border-red">
-          <div className="content-icon">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="w-8 h-8"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M21 7.5l-9-5.25L3 7.5m18 0l-9 5.25m9-5.25v9l-9 5.25M3 7.5l9 5.25M3 7.5v9l9 5.25m0-9v9"
-              />
-            </svg>
-          </div>
-          <h3 className="content-title font-semibold mt-4 mb-2">
-            Orchestration
-          </h3>
-          <span className="content-description">
-            Deploy our Orchestration template, or view a live example
-          </span>
-        </div>
-      </div>
-      <div className="col-span-12 md:col-span-6 lg:col-span-4">
-        <div className="card group flex flex-col p-6 rounded-lg justify-start overflow-hidden border border-red">
-          <div className="content-icon">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="w-8 h-8"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M21 7.5l-9-5.25L3 7.5m18 0l-9 5.25m9-5.25v9l-9 5.25M3 7.5l9 5.25M3 7.5v9l9 5.25m0-9v9"
-              />
-            </svg>
-          </div>
-          <h3 className="content-title font-semibold mt-4 mb-2">
-            Orchestration
-          </h3>
-          <span className="content-description">
-            Deploy our Orchestration template, or view a live example
-          </span>
-        </div>
-      </div>
-    </div>
+    <Link
+      href={href}
+      className="card group flex flex-col col-span-12 md:col-span-6 lg:col-span-4 p-6 rounded-lg justify-start overflow-hidden border border-gray-200 dark:border-gray-600"
+    >
+      <div className="content-icon">{icon}</div>
+      <h3 className="content-title font-semibold mt-4 mb-1">{title}</h3>
+      <span className="content-description">{description}</span>
+    </Link>
   );
 };
