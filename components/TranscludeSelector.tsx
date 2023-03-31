6a193ab4a76f6c5dd5f3ec136170a4f5db3d38f7
Keagan Pang <keagan.pang@y42.com>
2023-03-31 13:54:11 +0200

    added dropdown selector for transclusion component

--- components/TranscludeSelector.tsx
+++ components/TranscludeSelector.tsx
@@ -1,5 +1,6 @@
 import dynamic from "next/dynamic";
 import { useState } from "react";
+import { FontAwesomeIcon } from "./icons/FontAwesomeIcon";
 
 interface TranscludeSelectorProps {
   pages: string[];
@@ -7,12 +8,13 @@ interface TranscludeSelectorProps {
 
 export const TranscludeSelector = ({ pages }: TranscludeSelectorProps) => {
   const [selected, setSelected] = useState<string>(pages[0]);
+  const [isOpen, setIsOpen] = useState<boolean>(false);
 
-  //   useEffect(() => {
-  //     setArticles();
-  //   }, []);
+  const handleOpen = () => {
+    setIsOpen((prevState) => !prevState);
+  };
 
-  const Foo = dynamic(
+  const TranscludedContent = dynamic(
     () => import(`pages/trust-center/_subprocessors/${selected}`),
     {
       ssr: false,
@@ -20,21 +22,42 @@ export const TranscludeSelector = ({ pages }: TranscludeSelectorProps) => {
   );
 
   return (
-    <div className="">
-      <ol>
-        {pages.map((page, index) => {
-          return (
-            <li
-              className="cursor-pointer bg-red-200"
-              key={index}
-              onClick={() => setSelected(page)}
-            >
-              {page}
-            </li>
-          );
-        })}
-      </ol>
-      <Foo />
-    </div>
+    <section className="">
+      <button
+        onClick={handleOpen}
+        className="flex flex-row items-center justify-center bg-gray-100 border border-gray-300 text-primary rounded-md divide-x dark: divide-gray-300"
+        type="button"
+      >
+        <span className="py-2 px-4 border-gray-300 font-">{selected}</span>
+
+        <span className="py-3 px-4">
+          <FontAwesomeIcon
+            width={16}
+            height={16}
+            fill="white"
+            className="w-4 h-4 fill-gray-800 dark:fill-[white]"
+            symbol="chevron-down"
+          />
+        </span>
+      </button>
+      {isOpen && (
+        <div className="z-10 absolute" onMouseLeave={() => setIsOpen(false)}>
+          <ol aria-labelledby="dropdownHoverButton">
+            {pages.map((page, index) => {
+              return (
+                <li
+                  className="cursor-pointer bg-red-200"
+                  key={index}
+                  onClick={() => setSelected(page)}
+                >
+                  {page}
+                </li>
+              );
+            })}
+          </ol>
+        </div>
+      )}
+      <TranscludedContent />
+    </section>
   );
 };
