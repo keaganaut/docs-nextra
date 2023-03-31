158398fa1922dc4734c0405fbe60d55fc8989a32
Keagan Pang <keagan.pang@y42.com>
2023-03-31 19:06:53 +0200

    added example content files

--- components/TranscludeSelector.tsx
+++ components/TranscludeSelector.tsx
@@ -24,40 +24,61 @@ export const TranscludeSelector = ({ pages }: TranscludeSelectorProps) => {
 
   return (
     <section className="mt-8">
-      <div className="flex relative" onMouseLeave={() => setIsOpen(false)}>
+      {/* <span className="px-1 mb-24">Published date</span> */}
+      <div
+        className="flex relative justify-end"
+        onMouseLeave={() => setIsOpen(false)}
+      >
         <button
           onClick={handleOpen}
-          className="flex flex-row items-center w-48 h-12 bg-gray-100 border border-gray-300 text-primary rounded-md divide-x dark: divide-gray-300"
+          className="flex flex-row items-center dark:bg-gray-100 border border-gray-300 text-primary rounded-md mb-4 dark:hover:border-gray-400 dark:hover:bg-gray-200 transition"
           type="button"
         >
-          <div className="flex flex-col relative py-2 px-4 justify-center items-center mx-auto border-gray-300">
-            <span className="text-xs absolute -top-3 -left-0 font-medium dark:bg-gray-100 px-4">
-              Published date
+          <div className="flex space-x-3 items-center px-4 border-r dark:border-gray-300 py-2">
+            <span className="">
+              <FontAwesomeIcon
+                width={16}
+                height={16}
+                fill="white"
+                className="w-4 h-4 fill-gray-800 dark:fill-[white]"
+                symbol="clock-rotate-left"
+              />
             </span>
-            {parse(selected).name}
+            <div className="border-gray-300 font-medium">
+              {parse(selected).name}
+            </div>
           </div>
 
-          <span className="py-4 px-4">
+          <span className="px-2">
             <FontAwesomeIcon
               width={16}
               height={16}
               fill="white"
-              className="w-4 h-4 fill-gray-800 dark:fill-[white]"
-              symbol={isOpen ? "chevron-up" : "chevron-down"}
+              className={`w-4 h-4 fill-gray-800 dark:fill-[white] transition ease-in-out duration-200 ${
+                isOpen ? "rotate-180" : ""
+              }`}
+              symbol="chevron-down"
             />
           </span>
         </button>
         {isOpen && (
-          <div className="w-44 top-12 z-10 absolute bg-gray-100 border border-gray-300 text-secondary rounded-md">
+          <div className="w-48 top-12 z-10 absolute bg-gray-100 border border-gray-300 text-secondary rounded-md overflow-hidden shadow-md">
             <ol aria-labelledby="dropdownHoverButton">
               {pages.map((page, index) => {
                 return (
                   <li
-                    className="py-2 px-4 cursor-pointer hover:bg-red-300"
+                    className="flex items-center py-2 px-4 cursor-pointer dark:hover:bg-gray-300"
                     key={index}
                     onClick={() => setSelected(page)}
                   >
                     {parse(page).name}
+                    {index === 0 ? (
+                      <small className="py-[2px] px-[6px] ml-4 bg-purple-500 text-[white] dark:text-primary rounded-[6px] text-xs font-bold">
+                        Latest
+                      </small>
+                    ) : (
+                      ""
+                    )}
                   </li>
                 );
               })}
