164e301078140f0ae062e8a3cd8d79abacd8515a
Keagan Pang <keagan.pang@y42.com>
2023-04-09 14:42:19 +0200

    switched api reference page to redoc component in mdx

--- components/Redoc.tsx
+++ components/Redoc.tsx
@@ -8,26 +8,33 @@ export const Redoc = (props) => {
         options={{
           nativeScrollbars: false,
           disableSearch: true,
-          hideLoading: true,
+          hideLoading: false,
           hideDownloadButton: true,
           hideHostname: true,
           hideSchemaPattern: false,
           pathInMiddlePanel: true,
           theme: {
             colors: {
-              error: {main: "#e25353"},
-              success:{main: "#209661"},
-              gray:{},
+              error: { main: "#e25353" },
+              success: { main: "#209661" },
+              gray: {},
               primary: { main: "rgb(23, 24, 28)" },
-              http: { get: "#209661", post: "#0f8cc6", patch: "#d74cb6",delete: "#e25353"},
+              http: {
+                get: "#209661",
+                post: "#0f8cc6",
+                patch: "#d74cb6",
+                delete: "#e25353",
+              },
               responses: {
-                error: {tabTextColor:"#e25353"}, success:{tabTextColor:"#209661"},
+                error: { tabTextColor: "#e25353" },
+                success: { tabTextColor: "#209661" },
               },
               text: {
                 primary: "#d1d1d1",
                 secondary: "rgba(255, 255, 255, 0.32)",
               },
             },
+
             sidebar: {
               backgroundColor: "rgb(23, 24, 28)",
               textColor: "rgba(255, 255, 255, 0.64)",
@@ -36,12 +43,21 @@ export const Redoc = (props) => {
             rightPanel: {
               backgroundColor: "rgb(23, 24, 28)",
               textColor: "rgba(255, 255, 255, 0.64)",
+              width: "50%",
             },
-            fab: { backgroundColor: "#846dff" },
+
+            fab: {
+              backgroundColor: "#5720bc",
+              color: "rgba(255, 255, 255, 0.94)",
+            },
             schema: {
               typeTitleColor: "rgba(255, 255, 255, 0.64)",
               requireLabelColor: "#ca6a31",
+              typeNameColor: "rgba(255, 255, 255, 0.32)",
+              linesColor: "#6f25ec",
+              arrow: { color: "#6f25ec" },
             },
+
             typography: {
               code: { backgroundColor: "#1E2025", color: "#CC6C32" },
             },
