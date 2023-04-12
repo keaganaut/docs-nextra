1e1a76fc8a47bf5af99ebd87b96730b4221bc2da
Keagan Pang <keagan.pang@y42.com>
2023-04-12 16:32:25 +0200

    feat: add scribehow in help widget prototype

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,8 @@
 import "@code-hike/mdx/dist/index.css";
 import { Analytics } from "@vercel/analytics/react";
+
+// @ts-ignore
+import { HelpWidget } from "components/HelpWidget";
 import { AppProps } from "next/app";
 import "../styles/custom-ch.css";
 import "../styles/globals.css";
@@ -7,6 +10,7 @@ import "../styles/globals.css";
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <HelpWidget />
       <Component {...pageProps} />
       <Analytics debug={false} />
     </>
